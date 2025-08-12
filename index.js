2c1e8dffb8f5595378185aa2969aee99bdc97fcf
govind098 <govgukal5@gmail.com>
2025-08-12 23:57:10 +0530

    company

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://job-hunt-frontend-gsfd.onrender.com',
-    credentials: true
-};
+    origin:'https://job-hunt-frontend-seven.vercel.app/',
+    credentials:true
+}
 
 app.use(cors(corsOptions));
 
